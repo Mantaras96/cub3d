38a81106a1250dbadd5fa131cb461cacd4b554dd
Albert <albert.mantaras@gmail.com>
2022-10-19 20:01:54 +0200

    Norminette

--- src/key_move_vision.c
+++ src/key_move_vision.c
@@ -1,25 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_move_vision.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 19:57:57 by amantara          #+#    #+#             */
+/*   Updated: 2022/10/19 19:57:59 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3d.h"
 
-void move_right_vision(t_all *all)
+void	move_right_vision(t_all *all)
 {
-    double old_dir_x;
-    double old_plane_x;
-    old_dir_x = all->pos.dir_x;
-    all->pos.dir_x = all->pos.dir_x * cos(-all->pos.rot_speed) - all->pos.dir_y * sin(-all->pos.rot_speed);
-    all->pos.dir_y = old_dir_x * sin(-all->pos.rot_speed) + all->pos.dir_y * cos(-all->pos.rot_speed);
-    old_plane_x = all->pos.plane_x;
-    all->pos.plane_x = all->pos.plane_x * cos(-all->pos.rot_speed) - all->pos.plane_y * sin(-all->pos.rot_speed);
-    all->pos.plane_y = old_plane_x * sin(-all->pos.rot_speed) + all->pos.plane_y * cos(-all->pos.rot_speed);
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = all->pos.dir_x;
+	all->pos.dir_x = all->pos.dir_x * cos(-all->pos.rot_speed)
+		- all->pos.dir_y * sin(-all->pos.rot_speed);
+	all->pos.dir_y = old_dir_x * sin(-all->pos.rot_speed)
+		+ all->pos.dir_y * cos(-all->pos.rot_speed);
+	old_plane_x = all->pos.plane_x;
+	all->pos.plane_x = all->pos.plane_x * cos(-all->pos.rot_speed)
+		- all->pos.plane_y * sin(-all->pos.rot_speed);
+	all->pos.plane_y = old_plane_x * sin(-all->pos.rot_speed)
+		+ all->pos.plane_y * cos(-all->pos.rot_speed);
 }
 
-void move_left_vision(t_all *all)
+void	move_left_vision(t_all *all)
 {
-    double old_dir_x;
-    double old_plane_x;
-    old_dir_x = all->pos.dir_x;
-    all->pos.dir_x = all->pos.dir_x * cos(all->pos.rot_speed) - all->pos.dir_y * sin(all->pos.rot_speed);
-    all->pos.dir_y = old_dir_x * sin(all->pos.rot_speed) + all->pos.dir_y * cos(all->pos.rot_speed);
-    old_plane_x = all->pos.plane_x;
-    all->pos.plane_x = all->pos.plane_x * cos(all->pos.rot_speed) - all->pos.plane_y * sin(all->pos.rot_speed);
-    all->pos.plane_y = old_plane_x * sin(all->pos.rot_speed) + all->pos.plane_y * cos(all->pos.rot_speed);
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = all->pos.dir_x;
+	all->pos.dir_x = all->pos.dir_x * cos(all->pos.rot_speed)
+		- all->pos.dir_y * sin(all->pos.rot_speed);
+	all->pos.dir_y = old_dir_x * sin(all->pos.rot_speed)
+		+ all->pos.dir_y * cos(all->pos.rot_speed);
+	old_plane_x = all->pos.plane_x;
+	all->pos.plane_x = all->pos.plane_x * cos(all->pos.rot_speed)
+		- all->pos.plane_y * sin(all->pos.rot_speed);
+	all->pos.plane_y = old_plane_x * sin(all->pos.rot_speed)
+		+ all->pos.plane_y * cos(all->pos.rot_speed);
 }
--- src/main.c
+++ src/main.c
@@ -6,45 +6,47 @@
 /*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 23:22:17 by albertmanta       #+#    #+#             */
-/*   Updated: 2022/09/30 16:49:39 by amantara         ###   ########.fr       */
+/*   Updated: 2022/10/19 20:01:34 by amantara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int count_file_lines(t_global *global, int i)
+int	count_file_lines(t_global *global, int i)
 {
-	while(global->map[i])
+	while (global->map[i])
 	{
-		if(global->map[i][0] == '\n')
+		if (global->map[i][0] == '\n')
 		{
 			break ;
 		}
 		i++;
 	}
-	return(i);
+	return (i);
 }
 
-int count_empty_lines(t_global *global, int i, int count)
+int	count_empty_lines(t_global *global, int i, int count)
 {
-	if (global->map[i][0] == '\n'){
-		while(global->map[i])
+	if (global->map[i][0] == '\n')
+	{
+		while (global->map[i])
 		{
-			if(global->map[i][0] == '\n')
+			if (global->map[i][0] == '\n')
 			{
 				i++;
 				count++;
 			}
-			else 
+			else
 				break ;
 		}
 	}
 	return (count);
 }
 
-int count_lines(t_global *global)
+int	count_lines(t_global *global)
 {
-	int i;
+	int	i;
+
 	i = 0;
 	global->lines_textures = count_file_lines(global, i);
 	global->empty_line = count_empty_lines(global, global->lines_textures, 0);
@@ -52,36 +54,36 @@ int count_lines(t_global *global)
 	global->lines_colors = i - global->empty_line - global->lines_textures;
 	global->empty_line_2 = count_empty_lines(global, i, 0);
 	i = count_file_lines(global, global->empty_line_2 + i);
-	global->lines_maps = i - (global->lines_textures + global->lines_colors + global->empty_line + global->empty_line_2);
+	global->lines_maps = i - (global->lines_textures
+			+ global->lines_colors + global->empty_line + global->empty_line_2);
 	if (global->lines_textures < 4 || global->lines_textures > 4)
 		return (0);
 	if (global->lines_colors < 2 || global->lines_colors > 2)
 		return (0);
-	 return(1);
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	t_all all;
+	t_all	all;
 
 	if (argc == 2)
 	{
 		validate_and_read_map(argv[1], &all);
-		if(!count_lines(&all.global))
+		if (!count_lines(&all.global))
 			show_error_msg(1, "Error numero de texturas\n");
-		if(!split_map(&all.global))
+		if (!split_map(&all.global))
 			return (0);
 		validate_textures(&all.global);
 		validate_colors(&all.global);
 		ft_directions(&all);
 		init_data(&all);
-
-		//validate_map(&all.global);
 		all.global.mlx = mlx_init();
 		all.global.win = mlx_new_window(all.global.mlx, WIDTH, HEIGHT, "cub3d");
 		all.global.img = mlx_new_image(all.global.mlx, WIDTH, HEIGHT);
-		all.global.addr = mlx_get_data_addr(all.global.img, &all.global.bits_per_pixel, &all.global.line_length, &all.global.endian);
-
+		all.global.addr = mlx_get_data_addr(all.global.img,
+				&all.global.bits_per_pixel, &all.global.line_length,
+				&all.global.endian);
 		ft_texture(&all);
 		mlx_hook(all.global.win, 02, (0L << 0), mover, &all);
 		mlx_hook(all.global.win, 03, (0L << 0), release, &all);
@@ -96,5 +98,3 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
-
-
