452b5660fc2e26737e08e3dbfcfa8f8273e5e17c
Albert <albert.mantaras@gmail.com>
2022-10-19 20:09:48 +0200

    Ultimo norminette faltan lineas

--- src/ft_directions.c
+++ src/ft_directions.c
@@ -1,46 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_directions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 20:08:35 by amantara          #+#    #+#             */
+/*   Updated: 2022/10/19 20:08:36 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3d.h"
 
-
-void ft_directions(t_all *all)
+void	ft_directions(t_all *all)
 {
-    all->corde.direction = 'E';
-    all->corde.x = 12;
-    all->corde.y = 5;
-
-    if (all->corde.direction == 'N')
-    {
-        all->pos.pos_x = all->corde.x;
-	    all->pos.pos_y = all->corde.y;
-  	    all->pos.dir_y = 0;
-	    all->pos.dir_x = 1;
-	    all->pos.plane_x = 0;
-	    all->pos.plane_y = -0.66;
-    }
-    if (all->corde.direction == 'S')
-    {
-        all->pos.pos_x = all->corde.x;
-	    all->pos.pos_y = all->corde.y;
-  	    all->pos.dir_y = 0;
-	    all->pos.dir_x = -1;
-	    all->pos.plane_x = 0;
-	    all->pos.plane_y = 0.66;
-    }
-    if (all->corde.direction == 'W')
-    {
-        all->pos.pos_x = all->corde.x;
-	    all->pos.pos_y = all->corde.y;
-  	    all->pos.dir_y = 1;
-	    all->pos.dir_x = 0;
-	    all->pos.plane_x = 0.66;
-	    all->pos.plane_y = 0;
-    }
-    if (all->corde.direction == 'E')
-    {
-        all->pos.pos_x = all->corde.x;
-	    all->pos.pos_y = all->corde.y;
-  	    all->pos.dir_y = -1;
-	    all->pos.dir_x = 0;
-	    all->pos.plane_x = -0.66;
-	    all->pos.plane_y = 0;
-    }
+	all->corde.direction = 'E';
+	all->corde.x = 12;
+	all->corde.y = 5;
+	if (all->corde.direction == 'N')
+	{
+		all->pos.pos_x = all->corde.x;
+		all->pos.pos_y = all->corde.y;
+		all->pos.dir_y = 0;
+		all->pos.dir_x = 1;
+		all->pos.plane_x = 0;
+		all->pos.plane_y = -0.66;
+	}
+	if (all->corde.direction == 'S')
+	{
+		all->pos.pos_x = all->corde.x;
+		all->pos.pos_y = all->corde.y;
+		all->pos.dir_y = 0;
+		all->pos.dir_x = -1;
+		all->pos.plane_x = 0;
+		all->pos.plane_y = 0.66;
+	}
+	if (all->corde.direction == 'W')
+	{
+		all->pos.pos_x = all->corde.x;
+		all->pos.pos_y = all->corde.y;
+		all->pos.dir_y = 1;
+		all->pos.dir_x = 0;
+		all->pos.plane_x = 0.66;
+		all->pos.plane_y = 0;
+	}
+	if (all->corde.direction == 'E')
+	{
+		all->pos.pos_x = all->corde.x;
+		all->pos.pos_y = all->corde.y;
+		all->pos.dir_y = -1;
+		all->pos.dir_x = 0;
+		all->pos.plane_x = -0.66;
+		all->pos.plane_y = 0;
+	}
 }
--- src/key_move.c
+++ src/key_move.c
@@ -1,49 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_move.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 20:05:45 by amantara          #+#    #+#             */
+/*   Updated: 2022/10/19 20:06:12 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3d.h"
-void move_up(t_all *all)
+
+void	move_up(t_all *all)
 {
-    if (all->global.nums[(int)(all->pos.pos_x + all->pos.dir_x * all->pos.move_speed)][((int)all->pos.pos_y)] == '0')
-        all->pos.pos_x += all->pos.dir_x * all->pos.move_speed;
-    if (all->global.nums[((int)all->pos.pos_x)][(int)(all->pos.pos_y + all->pos.dir_x * all->pos.move_speed)] == '0')
-        all->pos.pos_y += all->pos.dir_y * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_x + all->pos.dir_x
+			* all->pos.move_speed)][((int)all->pos.pos_y)] == '0')
+		all->pos.pos_x += all->pos.dir_x * all->pos.move_speed;
+	if (all->global.nums[((int)all->pos.pos_x)]
+		[(int)(all->pos.pos_y + all->pos.dir_x * all->pos.move_speed)] == '0')
+		all->pos.pos_y += all->pos.dir_y * all->pos.move_speed;
 }
 
-void move_down(t_all *all)
+void	move_down(t_all *all)
 {
-    if (all->global.nums[(int)(all->pos.pos_x - all->pos.dir_x * all->pos.move_speed)][((int)all->pos.pos_y)] == '0')
-        all->pos.pos_x -= all->pos.dir_x * all->pos.move_speed;
-    if (all->global.nums[((int)all->pos.pos_x)][(int)(all->pos.pos_y - all->pos.dir_x * all->pos.move_speed)] == '0')
-        all->pos.pos_y -= all->pos.dir_y * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_x - all->pos.dir_x
+			* all->pos.move_speed)][((int)all->pos.pos_y)] == '0')
+		all->pos.pos_x -= all->pos.dir_x * all->pos.move_speed;
+	if (all->global.nums[((int)all->pos.pos_x)]
+		[(int)(all->pos.pos_y - all->pos.dir_x * all->pos.move_speed)] == '0')
+		all->pos.pos_y -= all->pos.dir_y * all->pos.move_speed;
 }
 
-void move_right(t_all *all)
+void	move_right(t_all *all)
 {
-    if (all->global.nums[(int)(all->pos.pos_x)][((int)(all->pos.pos_y -  all->pos.dir_x * all->pos.move_speed))] == '0')
-        all->pos.pos_y -= all->pos.dir_x * all->pos.move_speed;
-    if (all->global.nums[(int)(all->pos.pos_y + all->pos.dir_x * all->pos.move_speed)][(int)(all->pos.pos_y)] == '0')
-        all->pos.pos_x += all->pos.dir_y * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_x)]
+		[((int)(all->pos.pos_y - all->pos.dir_x * all->pos.move_speed))] == '0')
+		all->pos.pos_y -= all->pos.dir_x * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_y
+			+ all->pos.dir_x * all->pos.move_speed)]
+			[(int)(all->pos.pos_y)] == '0')
+		all->pos.pos_x += all->pos.dir_y * all->pos.move_speed;
 }
 
-void move_left(t_all *all)
+void	move_left(t_all *all)
 {
-    if (all->global.nums[(int)(all->pos.pos_x)][((int)(all->pos.pos_y +  all->pos.dir_x * all->pos.move_speed))] == '0')
-        all->pos.pos_y += all->pos.dir_x * all->pos.move_speed;
-    if (all->global.nums[(int)(all->pos.pos_y - all->pos.dir_x * all->pos.move_speed)][(int)(all->pos.pos_y)] == '0')
-        all->pos.pos_x -= all->pos.dir_y * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_x)]
+		[((int)(all->pos.pos_y + all->pos.dir_x * all->pos.move_speed))] == '0')
+		all->pos.pos_y += all->pos.dir_x * all->pos.move_speed;
+	if (all->global.nums[(int)(all->pos.pos_y
+			- all->pos.dir_x * all->pos.move_speed)]
+			[(int)(all->pos.pos_y)] == '0')
+		all->pos.pos_x -= all->pos.dir_y * all->pos.move_speed;
 }
 
-int key_move (t_all *all)
+int	key_move(t_all *all)
 {
-    if (all->corde.w == 1)
-        move_up(all);
-    if (all->corde.s == 1)
-        move_down(all);
-    if (all->corde.d == 1)
-        move_right(all);
-    if (all->corde.a == 1)
-        move_left(all);
-    if (all->corde.right == 1)
-        move_right_vision(all);
-    if (all->corde.left == 1)
-        move_left_vision(all);
-    return (0);
+	if (all->corde.w == 1)
+		move_up(all);
+	if (all->corde.s == 1)
+		move_down(all);
+	if (all->corde.d == 1)
+		move_right(all);
+	if (all->corde.a == 1)
+		move_left(all);
+	if (all->corde.right == 1)
+		move_right_vision(all);
+	if (all->corde.left == 1)
+		move_left_vision(all);
+	return (0);
 }
