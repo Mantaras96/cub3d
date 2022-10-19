78198d2d30cd8311b2fb14135c76fb6aede6cb5d
Albert <albert.mantaras@gmail.com>
2022-10-19 19:05:46 +0200

    Validate map 2

--- src/validateMap2.c
+++ src/validateMap2.c
@@ -6,28 +6,31 @@
 /*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 18:50:24 by amantara          #+#    #+#             */
-/*   Updated: 2022/10/19 18:59:22 by amantara         ###   ########.fr       */
+/*   Updated: 2022/10/19 19:05:34 by amantara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void    validate_first_last_row(t_global *global)
+void	validate_first_last_row(t_global *global)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	j = 0;
-	while(global->nums[0][j]){
-		if (global->nums[0][j] != 32 && global->nums[0][j] != '1' && global->nums[0][j] != '\n' && global->nums[0][j] != '\t') // Se puede quitar el \n si se hace un split antes.
-			printf("Error primera linea"); 
+	while (global->nums[0][j])
+	{
+		if (global->nums[0][j] != 32 && global->nums[0][j] != '1'
+			&& global->nums[0][j] != '\n' && global->nums[0][j] != '\t')
+			printf("Error primera linea");
 		j++;
 	}
-
 	j = 0;
 	i = ft_matrix_len(global->nums) - 1;
-	while(global->nums[i][j]){
-		if (global->nums[i][j] != 32 && global->nums[i][j] != '1' && global->nums[i][j] != '\n' && global->nums[i][j] != '\t') // Se puede quitar el \n si se hace un split antes.
+	while (global->nums[i][j])
+	{
+		if (global->nums[i][j] != 32 && global->nums[i][j] != '1'
+			&& global->nums[i][j] != '\n' && global->nums[i][j] != '\t')
 			printf("Error ultima linea");
 		j++;
 	}
@@ -40,10 +43,13 @@ int	is_space_tab(char c)
 	return (0);
 }
 
-void check_directions(int i, int j, t_global *global){
-	if (is_space_tab(global->nums[i - 1][j]) || is_space_tab(global->nums[i + 1][j]) || is_space_tab(global->nums[i][j - 1]) || is_space_tab(global->nums[i][j + 1])){
+void	check_directions(int i, int j, t_global *global)
+{
+	if (is_space_tab(global->nums[i - 1][j])
+		|| is_space_tab(global->nums[i + 1][j])
+		|| is_space_tab(global->nums[i][j - 1])
+		|| is_space_tab(global->nums[i][j + 1]))
 		printf("Error posicion[%d][%d]", i, j);
-	} 
 }
 
 void	validate_all_different_wall(t_global *global)
@@ -52,10 +58,14 @@ void	validate_all_different_wall(t_global *global)
 	int	j;
 
 	i = 1;
-	while (global->nums[i]){
+	while (global->nums[i])
+	{
 		j = 0;
-		while (global->nums[i][j]){
-			if (global->nums[i][j] == '0' || global->nums[i][j] == 'N' || global->nums[i][j] == 'S' || global->nums[i][j] == 'E'  || global->nums[i][j] == 'W')
+		while (global->nums[i][j])
+		{
+			if (global->nums[i][j] == '0' || global->nums[i][j] == 'N'
+				|| global->nums[i][j] == 'S'
+				|| global->nums[i][j] == 'E' || global->nums[i][j] == 'W')
 				check_directions(i, j, global);
 			j++;
 		}
